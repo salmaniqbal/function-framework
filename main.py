c7df0b2c4d5d017d1a9dec0b7747bd3cd56629fc
Salman Iqbal <salman.iqbal1@yahoo.com>
2020-04-02 10:27:36 +0100

    changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def hello_http(request):
         request (flask.Request): The request object.
         <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
     Returns:
-        The response text, or any set of values that can be turned into a
+        The response text, or any set of values that cana be turned into a
         Response object using `make_response`
         <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
     """
